062e80e132e8690cd492c2e43b2092a92ae05a9f
Jae Lee <nagneeve@gmail.com>
2015-11-10 17:52:51 -0700

    ...

--- pattern_recognition/cmake_install.cmake
+++ pattern_recognition/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/jae/src/github/cs650/pattern_recognition
+# Install script for directory: /home/magiccjae/src/github/cs650/pattern_recognition
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/jae/src/github/cs650/pattern_recognition/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/magiccjae/src/github/cs650/pattern_recognition/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/jae/src/github/cs650/pattern_recognition/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/magiccjae/src/github/cs650/pattern_recognition/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
--- pattern_recognition/main.cpp
+++ pattern_recognition/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <set>
 
 using namespace cv;
 using namespace std;
@@ -12,35 +13,65 @@ int b_background;
 int g_background;
 int r_background;
 
-void connected_component(int row, int col);
+Mat connected_component(int row, int col);
+int find_howmany(Mat connected_image, int row, int col);
+void cal_compactness(Mat connected_image, int row, int col);
 
 int main(){
 	
-	image = image = imread("shapes.pgm");
+    image = image = imread("testshapes.pgm");
     if(! image.data ){                              // Check for invalid input
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
+    
+    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
     int row = image.rows;
     int col = image.cols;
     cout << row << " x " << col << " image" << endl;
+    
     b_background = (int)image.at<Vec3b>(0,0)[0];
     g_background = (int)image.at<Vec3b>(0,0)[1];
     r_background = (int)image.at<Vec3b>(0,0)[2];
     cout << "background rgb  " << b_background << " " << g_background << " " << r_background << endl;
-    connected_component(row, col);
+    
+    Mat connected_image;
+    
+    connected_image = connected_component(row, col);
+    namedWindow( "connected", WINDOW_AUTOSIZE );
+    imshow( "connected", connected_image );
+    
+    int howmany = find_howmany(connected_image,row,col);
+    cout << "the number of shapes:  " << howmany << endl;
 
-	namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", image );                   // Show our image inside it.
+    
 
     waitKey(0);                                          // Wait for a keystroke in the window
-
 	
-	return 0;
+    return 0;
 
 }
 
-void connected_component(int row, int col){
+void cal_compactness(Mat connected_image){
+    
+}
+
+int find_howmany(Mat connected_image, int row, int col){
+    set<int> myset;
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            if(connected_image.at<Vec3b>(i,j)[0] != 0){
+                myset.insert(connected_image.at<Vec3b>(i,j)[0]);
+            }
+        }
+    }
+    return myset.size();
+
+}
+
+Mat connected_component(int row, int col){
     map<int,int> mymap;
     int label_array[row][col];
     for(int i = 0; i < row; i++){
@@ -124,13 +155,18 @@ void connected_component(int row, int col){
     
     for(int i = 0; i < row; i++){
         for(int j = 0; j < col; j++){
-            check.at<Vec3b>(i,j)[0] = label_array[i][j];
-            check.at<Vec3b>(i,j)[1] = label_array[i][j];
-            check.at<Vec3b>(i,j)[2] = label_array[i][j];
-
+            if(label_array[i][j] != 0){
+                check.at<Vec3b>(i,j)[0] = 255-label_array[i][j];
+                check.at<Vec3b>(i,j)[1] = 255;
+                check.at<Vec3b>(i,j)[2] = label_array[i][j];
+            }
+            else{
+                check.at<Vec3b>(i,j)[0] = 0;
+                check.at<Vec3b>(i,j)[1] = 0;
+                check.at<Vec3b>(i,j)[2] = 0;
+            }
         }
     }
-	namedWindow( "check", WINDOW_AUTOSIZE );
-    imshow( "check", check );
 
+    return check;
 }
